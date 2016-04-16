0127e85998c597a05a00f87f9094f37cdb063a6b
macgeek30 <mike25ko@gmail.com>
2016-04-16 14:17:31 -0600

    Done

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,39 @@
 describe('UserService', function () {
+  beforeEach(module('app'));
 
-});
+  var UserService, $httpBackend;
+
+  beforeEach(inject(function($injector){
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend.when('GET', '/rest/user').respond({first_name: "Will", last_name: "Smith"});
+  }));
+
+  it('should return user first_name and user last_name', function(done) {
+    $httpBackend.expectGET('/rest/user');
+
+    UserService.getUser().then(function(response) {
+      var user = response.data;
+      if (user.first_name === "Will" && user.last_name === "Smith"){
+        done();
+      }
+    });
+
+    $httpBackend.flush();
+  });
+
+  it('should append first and last name to create fullName', function(done){
+    $httpBackend.expectGET('/rest/user');
+
+    UserService.getUser().then(function(response) {
+      var user = response.data;
+      if (UserService.createFullName(user) === "Will Smith") {
+        done();
+      }
+    });
+
+    $httpBackend.flush();
+  });
+
+  });
