f1027474fbe5ef21f34a3d1c8a864b297b780f27
febbraiod <donedesign1@gmail.com>
2016-05-10 14:04:40 -0400

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,38 @@
 describe('UserService', function () {
+  beforeEach(module('app'));
+
+  var UserService;
+  var $httpBackend;
+
+  beforeEach(inject(function($injector){
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend.when('GET', '/rest/user').respond({user: 'Don', email: 'don@don.com'});
+
+  }));
+
+  it('should return user info', function(done){
+    $httpBackend.expectGET('/rest/user');
+
+    UserService.getUser().then(function(resp){
+      var data = resp.data;
+      if(data.user === 'Don' && data.email === 'don@don.com'){
+        done();
+      }
+    });
+
+    $httpBackend.flush();
+  });
+
+  var me = {
+    first_name: "Don",
+    last_name: "Febbraio"
+  };
+
+  it('should return users fullname', function(){
+    expect(UserService.createFullName(me)).toEqual('Don Febbraio');
+  });
+
 
 });
