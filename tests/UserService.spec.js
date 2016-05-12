455b5173b85015ac9ae0dc968c91112c4b469d8f
MitulMistry <MitulMistryDev@gmail.com>
2016-05-12 00:14:42 -0500

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,32 @@
 describe('UserService', function () {
+  beforeEach(module('app'));
+
+  var UserService;
+
+  beforeEach(inject(function($injector){
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend.when('GET', '/rest/user').respond({first_name: 'Bob', last_name: 'Smith', email: 'bob@bob.com'});
+  }));
+
+  it('should get the current users info', function (done) {
+    $httpBackend.expectGET('/rest/user');
+
+    UserService
+    .getUser()
+    .then(function (res) {
+      var data = res.data
+      if (data.email === 'bob@bob.com' && data.first_name === 'Bob' && data.last_name === 'Smith') {
+        done();
+      }
+    });
+
+    $httpBackend.flush();
+  });
+
+  it('should add names together correctly', function () {
+    expect(UserService.createFullName({first_name: 'Bob', last_name: 'Smith'})).toEqual('Bob Smith');
+  });
 
 });
