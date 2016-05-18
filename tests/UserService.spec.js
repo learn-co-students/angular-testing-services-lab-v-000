e0400f219f8f16cfdcf797c8d832deaf854a352d
jsgoldb <jgoldb@yahoo.com>
2016-05-18 11:27:09 -0500

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,31 @@
 describe('UserService', function () {
+  beforeEach(module('app'));
+
+  var UserService, $httpBackend;
+
+  beforeEach(inject(function($injector) {
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend.when('GET', '/rest/user').respond({user: 'Tom Haverford'});
+  }));
+
+  it('should get the users info', function () {
+    $httpBackend.expectGET('/rest/user');
+
+    UserService
+      .getUser()
+      .then(function (res) {
+        if (res.user === 'Tom Haverford'){
+          done();
+        }
+      });
+      $httpBackend.flush();
+  });
+
+  it('should output a users name correctly', function(){
+    var user = {first_name: 'Bill', last_name: 'Clinton'};
+    expect(UserService.createFullName(user)).toEqual('Bill Clinton');
+  });
 
 });
