0d39e8275782cc558582aaae2aecaf5ab914dc0d
Brian Holland <beehollander@gmail.com>
2016-05-19 11:28:04 -0500

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,38 @@
-describe('UserService', function () {
+describe('UserService', function() {
+  beforeEach(module('app'));
 
+  var UserService, $httpBackend;
+
+  beforeEach(inject(function($injector) {
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend.when('GET', '/rest/user').respond({
+
+      first_name: 'Bill',
+      last_name: 'Gates',
+      email: 'bill@microsoft.com'});
+  }));
+
+  it('should get the current users information', function(done) {
+    $httpBackend.expectGET('/rest/user');
+
+    UserService
+      .getUser()
+      .then(function (res) {
+        var user = res.data;
+        if (user.email === 'bill@microsoft.com' && user.last_name === 'Gates') {
+          done();
+        }
+      });
+
+    $httpBackend.flush();
+  });
+
+  it('should return a human-readable name (e.g.: Tom Thompson)', function() {
+    var fullname = UserService
+      .createFullName({first_name: 'John', last_name: 'Johnson'});
+
+    expect(fullname).toEqual('John Johnson');
+  });
 });
