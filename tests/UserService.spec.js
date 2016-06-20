008ed9791700c7e16939eba08152cf6c28f653b0
Tyler Smith <oats000@hotmail.com>
2016-06-20 23:42:35 +0000

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,27 @@
 describe('UserService', function () {
+    beforeEach(module('app'));
+
+    var UserService, $httpBackend;
+
+    beforeEach(inject(function ($injector) {
+        UserService = $injector.get('UserService');
+        $httpBackend = $injector.get('$httpBackend');
+
+        $httpBackend.when('GET', '/rest/user').respond({user: 'Bill Gates', email: 'bill@microsoft.com'});
+    }));
+
+    it('should get the current users information', function (done) {
+        $httpBackend.expectGET('/rest/user');
+
+        UserService
+          .getUser()
+          .then(function (res) {
+            if (res.email === 'bill@microsoft.com' && res.createFullName === 'Bill Gates') {
+              done();
+            }
+          });
+
+        $httpBackend.flush();
+    });
 
 });
