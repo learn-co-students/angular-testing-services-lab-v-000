150a1550e8645773864e2c89adac6448f77d3a86
Dakota Martinez <dakotaleemusic@gmail.com>
2016-06-23 21:15:12 +0000

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,32 @@
 describe('UserService', function () {
-
+  beforeEach(module('app'));
+  
+  var UserService, $httpBackend;
+  
+  beforeEach(inject(function($injector){
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+    
+    $httpBackend
+      .when('GET', '/rest/user')
+      .respond({
+        first_name: 'Dakota', 
+        last_name: 'Martinez'
+      });
+  }));
+  
+  it('should respond with a users information', function(done){
+    $httpBackend.expectGET('/rest/user');
+    
+    UserService
+      .getUser()
+      .then(function(response){
+        var data = response.data;
+        if (data.first_name === 'Dakota' && data.last_name === 'Martinez') {
+          done();  
+        }
+      });
+      
+    $httpBackend.flush();
+  });
 });
