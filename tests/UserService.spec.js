6927ca99a4ff85a40a53efd94ad8f298ad17801b
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-06-24 13:06:46 -0500

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,30 @@
 describe('UserService', function () {
+  beforeEach(module('app'));
 
+  var UserService;
+  var $httpBackend;
+
+  beforeEach(inject(function($injector){
+    UserService = $injector.get("UserService");
+    $httpBackend = $injector.get("$httpBackend");
+
+    $httpBackend.when("GET", '/rest/user').respond({first_name: "Frank",last_name: "Bangles"});
+  }));
+
+  it("get users's info",function(done){
+    $httpBackend.expectGET("/rest/user");
+
+    UserService
+      .getUser()
+      .then(function(res){
+        var data = res.data;
+        if(data.first_name === "Frank" && data.last_name === "Bangles"){
+          done();
+        };
+      });
+      $httpBackend.flush();
+  });
+  it("full name",function(){
+    expect(UserService.createFullName({first_name: "Frank", last_name: "Bangles"})).toEqual("Frank Bangles");
+  });
 });
