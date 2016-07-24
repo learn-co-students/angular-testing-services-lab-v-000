836c3413f7c0eab645a71c1203a68fda8e675bb5
Katrina McNeely <katrina@Katrinas-MacBook-Pro.local>
2016-07-23 23:52:49 -0400

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,32 @@
 describe('UserService', function () {
 
+  beforeEach(module('app'));
+
+  var UserService;
+
+  beforeEach(inject(function($injector){
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+  $httpBackend.when('GET', '/rest/user').respond({first_name: 'Ryan', last_name: 'McNeely'})
+}));
+
+  it('Should make correct $http requests', function(done) {
+    $httpBackend.expectGET('/rest/user')
+    UserService
+      .getUser()
+      .then(function (res){
+        var data = res.data
+        if (data.first_name === 'Ryan' && data.last_name === 'McNeely'){
+        done()
+      }
+    });
+
+    $httpBackend.flush();
+
+  });
+
+  it('Should have an injection that that creates a full name', function(){
+    expect(UserService.createFullName({first_name: 'Ryan', last_name: 'McNeely'})).toEqual('Ryan McNeely')
+  });
 });
