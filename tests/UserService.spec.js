e34b92d088550e0c6bfcc8031d8cf169c6133349
Peter Jacobson <github email address>
2016-08-15 16:22:19 -0400

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -10,6 +10,26 @@ describe('UserService', function () {
         $httpBackend.when('GET', '/rest/user').respond({user: 'Peter Jacobson', email: 'peter.jacobson@gmail.com'});
     }));
 
+
+    it('should get the current users information', function (done) {
+    $httpBackend.expectGET('/rest/user');
+
+    UserService
+      .getUser()
+      .then(function (res) {
+        var data = res.data;
+        if (data.email === 'bill@microsoft.com' && data.first_name === 'Bill') {
+          done();
+        }
+      });
+
+    $httpBackend.flush();
+  });
+
+  it('should add names together correctly', function () {
+    expect(UserService.createFullName({first_name: 'Test', last_name: 'Test'})).toEqual('Test Test');
+  });
+
 });
 
 
