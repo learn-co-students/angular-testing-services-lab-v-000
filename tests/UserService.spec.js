7934a2bfbd1c6e550e86d87e6f0dd47a9ad3ef5b
Peter Jacobson <github email address>
2016-08-15 16:21:09 -0400

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,15 @@
 describe('UserService', function () {
+   beforeEach(module('app'));
+
+   var UserService, $httpBackend;
+
+    beforeEach(inject(function ($injector) {
+        UserService = $injector.get('UserService');
+        $httpBackend = $injector.get('$httpBackend');
+ 
+        $httpBackend.when('GET', '/rest/user').respond({user: 'Peter Jacobson', email: 'peter.jacobson@gmail.com'});
+    }));
 
 });
+
+
