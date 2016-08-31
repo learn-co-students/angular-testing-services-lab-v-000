4d5f835828af165bf153b45975b03d6ce287dc78
agdavid <antonio.g.david@gmail.com>
2016-08-31 11:34:50 -0400

    set root module for test

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,35 @@
 describe('UserService', function () {
 
+  //set the root module
+  beforeEach(module('app'));
+
 });
+
+
+
+// describe('UserService', function () {
+//     beforeEach(module('app'));
+ 
+//     var UserService, $httpBackend;
+ 
+//     beforeEach(inject(function ($injector) {
+//         UserService = $injector.get('UserService');
+//         $httpBackend = $injector.get('$httpBackend');
+ 
+//         $httpBackend.when('GET', '/rest/user').respond({user: 'Bill Gates', email: 'bill@microsoft.com'});
+//     }));
+ 
+//     it('should get the current users information', function (done) {
+//         $httpBackend.expectGET('/rest/user');
+ 
+//         UserService
+//           .getUserInfo()
+//           .then(function (res) {
+//             if (res.email === 'bill@microsoft.com' && res.user === 'Bill Gates') {
+//               done();
+//             }
+//           });
+ 
+//         $httpBackend.flush();
+//     });
+// });
