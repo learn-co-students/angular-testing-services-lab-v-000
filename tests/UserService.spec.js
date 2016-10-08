3e5ef641d40cf11663c6a60034df6d09438cb00a
Nathaniel Miller <dukeoflaser@gmail.com>
2016-10-08 06:48:31 -0700

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,36 @@
 describe('UserService', function () {
 
+  beforeEach(module('app'));
+
+  var UserService;
+
+  beforeEach(inject(function($injector) {
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend.when('GET', '/rest/user').respond(
+      { user: 'dukeoflaser', country: 'USSR' }
+    );
+  }));
+
+  it('should get the users data', function (done) {
+        $httpBackend.expectGET('/rest/user');
+
+        UserService
+          .getUser()
+          .then(function(response) {
+            var data = response.data;
+
+            if(data.user === 'dukeoflaser' && data.country === 'USSR') {
+              done();
+            }
+          });
+
+          $httpBackend.flush();
+    });
+
+    it('should add names together correctly', function () {
+      expect(UserService.createFullName({first_name: 'Viktor', last_name: 'Kuchenski'})).toEqual('Viktor Kuchenski');
+    });
+
 });
