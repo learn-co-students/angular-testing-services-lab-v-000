10f300f2e28709f4624b1aa920720d2ceec63fc1
sensei100 <jenanton01@gmail.com>
2016-10-15 18:47:34 -0400

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,29 @@
 describe('UserService', function () {
+  beforeEach(module('app'));
 
+  var UserService;
+
+  beforeEach(inject(function($injector) {
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend.when('GET', '/rest/user').respond({first_name: 'Jen', last_name: 'Anton', email: 'test@test.com'});
+  }));
+
+    it('should get the current users information', function(done) {
+      $httpBackend.expectGET('/rest/user');
+    
+    UserService
+      .getUser()
+      .then(function(res) {
+        var data = res.data;
+        if (data.email === 'test@test.com' && data.first_name === 'Jen' && data.last_name === 'Anton') {
+          done();
+        }
+      });
+      $httpBackend.flush();
+    });
+    it('should join the first and last names', function() {
+      expect(UserService.createFullName({first_name: 'Jen', last_name: 'Anton'})).toEqual('Jen Anton');
+    });
 });
