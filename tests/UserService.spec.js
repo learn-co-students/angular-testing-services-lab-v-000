7dde9ed1b2235722b5ba0dc5eac6a045d8dd554a
cmiller36 <m.calliemiller@gmail.com>
2016-10-25 22:46:04 -0400

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,27 @@
 describe('UserService', function () {
+  beforeEach(module('app'));
 
+  var UserService, $httpBackend;
+ 
+    beforeEach(inject(function ($injector) {
+        UserService = $injector.get('UserService');
+        $httpBackend = $injector.get('$httpBackend');
+
+    it('should get the current users information', function (done) {
+        $httpBackend.expectGET('/rest/user');
+
+    UserService
+        .getUserInfo()
+        .then(function (res) {
+          var data = res.data;
+          if (data.email === 'callie@email.com' && data.user === 'Callie') {
+              done();
+            }
+          });
+    $httpBackend.flush();
+    });
+
+    it('should correctly join first and last names', function() {
+      expect(UserService.createFulName({ first_name: 'Jon', last_name: 'Snow'})).toEqual('Jon Snow');
+    })
 });
