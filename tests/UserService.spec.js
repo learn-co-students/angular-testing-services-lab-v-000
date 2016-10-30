a0bebe2ffd6681ba264867a78bdc2261b9391f33
krisnotchris <krishenderson@gmail.com>
2016-10-30 18:19:40 -0500

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,27 @@
 describe('UserService', function () {
+  beforeEach(module('app'));
 
+  var UserService, $httpBackend;
+
+  beforeEach(inject(function($injector){
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+    it('Get logged in user information', function(done){
+      $httpBackend.expectGET('/rest/user');
+
+      UserService
+        .getUserInfo()
+        .then(function(res){
+          var data = res.data;
+          if (data.email === 'test@gmail.com' && data.user === 'Test'){
+            done();
+          }
+        });
+        $httpBackend.flush();
+    });
+    it('Join in the users first and last name', function(){
+      expect(UserService.createFullName({first_name: 'test', last_name: 'last'})).toEqual('test last');
+    })
+  }))
 });
