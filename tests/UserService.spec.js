f2a15fa462635d56e614ce836a75dfffc5d4c213
Joseph Lim <joseph.d.lim@gmail.com>
2016-11-16 09:27:19 -0500

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,31 @@
 describe('UserService', function () {
+    beforeEach(module('app'));
 
+    var UserService, $httpBackend;
+
+    beforeEach(inject(function ($injector) {
+        UserService = $injector.get('UserService');
+        $httpBackend = $injector.get('$httpBackend');
+
+        $httpBackend.when('GET', '/rest/user').respond({first_name: 'Garfield', last_name: 'Cat', email: 'g@odiegoestospace.com'});
+    }));
+
+    it('should get the current users information', function (done) {
+        $httpBackend.expectGET('/rest/user');
+
+        UserService
+          .getUser()
+          .then(function (res) {
+            var data = res.data;
+            if (data.email === 'g@odiegoestospace.com' && data.first_name === 'Garfield') {
+              done();
+            }
+          });
+      $httpBackend.flush();
+    });
+
+    it('should get the current users full name', function () {
+        var testUser = {first_name: 'Garfield', last_name: 'Cat', email: 'g@odiegoestospace.com'}
+        expect(UserService.createFullName(testUser)).toBe('Garfield Cat');
+    });
 });
