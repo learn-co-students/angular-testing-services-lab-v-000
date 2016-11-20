710cff03e300cd5c6eab7126898234252c4383da
github username <github email address>
2016-11-20 10:37:36 -0500

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,33 @@
 describe('UserService', function () {
+    beforeEach(module('app'));
+
+    var UserService, $httpBackend;
+
+    beforeEach(inject(function ($injector) {
+        UserService = $injector.get('UserService');
+        $httpBackend = $injector.get('$httpBackend');
+
+        $httpBackend.when('GET', '/rest/user').respond({user: 'Octavio', email: 'octavio@paz.com'});
+    }));
+
+    it('should get the current users information', function () {
+        $httpBackend.expectGET('/rest/user');
+
+        UserService
+          .getUser()
+          .then(function (res) {
+            if (res.email === 'octavio@paz.com' && res.user === 'Octavio') {
+              done();
+            }
+          });
+
+        $httpBackend.flush();
+    });
+
+
+
+    it('should join first and last name', function () {
+     expect(UserService.createFullName({first_name: 'Octavio', last_name: 'Paz'})).toEqual('Octavio Paz');
+   });
 
 });
