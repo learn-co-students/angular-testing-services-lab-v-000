eaf4382b6e6edcbcd9149f9c790071d2cbae375c
abadfish <kirsten.ofarrell@gmail.com>
2016-11-20 09:16:41 -0500

    tested data response

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,25 @@
 describe('UserService', function () {
+  beforeEach(module('app'));
 
+  var UserService, $httpBackend;
+
+  beforeEach(inject(function($injector) {
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend.when('GET', '/rest/user').respond({user: 'Gus'});
+  }));
+
+  it('should get the current user info', function(done) {
+    $httpBackend.expectGET('/rest/user');
+      UserService
+        .getUser()
+        .then(function(res) {
+          var data  = res.data;
+          if (data.user === 'Gus') {
+            done();
+          }
+        });
+      $httpBackend.flush();
+  });
 });
