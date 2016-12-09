b0addb5a08d41d3ac8af3fb93c35bba4cf3f13f9
jbok4 <jbok4@aol.com>
2016-12-09 15:57:36 -0500

    complete

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,36 @@
 describe('UserService', function () {
+  var UserService, $httpBackend;
 
+  beforeEach(module('app'));
+
+  beforeEach(inject(function ($injector) {
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend
+      .when('GET', '/rest/user')
+      .respond({
+        first_name: 'Bill',
+        last_name: 'Gates',
+        email: 'bill@microsoft.com'
+      });
+  }));
+
+  it('should get the current users information', function (done) {
+    $httpBackend.expectGET('/rest/user');
+
+    UserService
+      .getUser()
+      .then(function (res) {
+        var data = res.data;
+        if (data.first_name === 'Bill' && data.last_name === 'Gates' && data.email === 'bill@microsoft.com') {
+          done();
+        }
+      });
+
+    $httpBackend.flush();
+  });
+  it('should combine first name and last name together', function(){
+    expect(UserService.createFullName({first_name: 'Bill', last_name: 'Gates'})).toEqual('Bill Gates');
+  });
 });
