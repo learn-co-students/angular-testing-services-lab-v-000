9f97778be7de4409974ff0822325b994894051d7
SiamKing <thesiameseelectric@gmail.com>
2016-12-23 12:25:47 -0800

    toophie all night long

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,31 @@
 describe('UserService', function () {
+  beforeEach(module('app'));
+
+  var UserService, $httpBackend;
+
+  beforeEach(inject(function ($injector) {
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+  $httpBackend.when('GET', '/rest/user').respond({first_name: "Bobby", last_name: "Brown" });
+  }));
+
+  it('should get the current users information', function (done) {
+    $httpBackend.expectGET('/rest/user');
+
+    UserService
+      .getUser()
+      .then(function (res) {
+        var data = res.data;
+        if (data.first_name === "Bobby" && data.last_name === "Brown") {
+          done();
+        }
+      });
+    $httpBackend.flush();
+  });
+
+  it('should concatenate users first and last name', function() {
+    expect(UserService.createFullName({first_name: "Bobby", last_name: "Brown"})).toEqual('Bobby Brown');
+  });
 
 });
