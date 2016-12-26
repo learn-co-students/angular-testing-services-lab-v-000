b6cc92b1c21116503ccfac14f4ead5f669dea9aa
tpstar <hanlee12@gmail.com>
2016-12-26 13:27:25 -0600

    done

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,27 @@
 describe('UserService', function () {
+    beforeEach(module('app'));
 
+    var UserService, $httpBackend;
+
+    beforeEach(inject(function ($injector) {
+        UserService = $injector.get('UserService');
+        $httpBackend = $injector.get('$httpBackend');
+
+        $httpBackend.when('GET', '/rest/user').respond({first_name: 'Bill', last_name: 'Gates'});
+    }));
+
+    it('should get the current users information', function (done) {
+        $httpBackend.expectGET('/rest/user');
+
+        UserService
+          .getUser()
+          .then(function (res) {
+            var userFullName = UserService.createFullName(res.data);
+            if (userFullName === 'Bill Gates') {
+              done();
+            }
+          });
+
+        $httpBackend.flush();
+    });
 });
