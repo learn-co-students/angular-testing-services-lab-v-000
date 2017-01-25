73491554e68daa9650408c6f4467627c02a1af68
lynch16 <will.lynch91@gmail.com>
2017-01-25 18:01:33 -0500

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,32 @@
 describe('UserService', function () {
+  beforeEach(module('app'));
 
+  var UserService, $httpBackend;
+
+  var user = {first_name: 'Ugh', last_name: 'Derp'};
+
+  beforeEach(inject(function($injector) {
+    UserService = $injector.get('UserService');
+    $httpBackend = $injector.get('$httpBackend');
+
+    $httpBackend.when('GET','/rest/user').respond(user);
+  }));
+
+  it('should get the current user name', function(done){
+    $httpBackend.expectGET('/rest/user');
+
+    UserService
+      .getUser()
+      .then(function(res){
+        if (res.data.first_name === 'Ugh' && res.data.last_name === 'Derp'){
+          done();
+        }
+      });
+
+    $httpBackend.flush();
+  });
+
+  it('should get the user fullname', function(){
+    expect(UserService.createFullName(user)).toEqual('Ugh Derp');
+  });
 });
