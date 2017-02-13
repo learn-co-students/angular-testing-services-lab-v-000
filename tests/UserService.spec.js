4ec6e49935d54748e7a9d82265abb7eea744c044
hilaryml <hilyttle@gmail.com>
2017-02-13 17:11:37 -0500

    Done.

--- tests/UserService.spec.js
+++ tests/UserService.spec.js
@@ -1,3 +1,31 @@
 describe('UserService', function () {
+    beforeEach(module('app'));
 
+    var UserService, $httpBackend;
+
+    beforeEach(inject(function ($injector) {
+        UserService = $injector.get('UserService');
+        $httpBackend = $injector.get('$httpBackend');
+
+        $httpBackend.when('GET', '/rest/user').respond({first_name: 'Steve', last_name: 'Jobs'});
+    }));
+
+    it('should get the current users information', function (done) {
+        $httpBackend.expectGET('/rest/user');
+
+        UserService
+          .getUser()
+          .then(function (response) {
+            var data = response.data;
+            if (data.first_name === 'Steve' && data.last_name === 'Jobs') {
+              done();
+            }
+          });
+
+        $httpBackend.flush();
+    });
+
+    it('should add the names together correctly', function () {
+      expect(UserService.createFullName({first_name: 'Bill', last_name: 'Gates'})).toEqual('Bill Gates');
+    });
 });
